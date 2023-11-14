b3ef3bf5b2cf1931b9e3c1fc460757128d7b09d3
Makai1979 <144675972+Makai1979@users.noreply.github.com>
2023-11-14 23:51:09 +0100

    Update 14.11.2023 - 23:51 Uhr

--- BennysRechner/code.js
+++ BennysRechner/code.js
@@ -1,49 +1,101 @@
 // Variablen erstellen
 let zahlA = '';
 let zahlB = '';
+let Nummer1 = '';
+let Nummer2 = '';
 let operator = '';
-let aufgabe = 'Die Aufgabe ist: '
+let aufgabeA = '';
+let aufgabeRest = '';
+let aufgabe = '';
 let anzeige = document.querySelector('#ergebnisBereich');
 
 
-function appendOperation (nummern) {
-    anzeige.textContent = zahlA += nummern;
-    console.log(zahlA);
-
+function appendOperation(nummern) {
+    if (aufgabeA <= 0) {
+        anzeige.textContent = zahlA += nummern;
+    } else {
+        anzeige.textContent = aufgabeA += nummern;
+    }
+    console.log("Hier steht zahlA: " + zahlA + " und hier ist aufgabeA: " + aufgabeA);
 }
 
-function pruefung (operator) {
+function pruefung(operator) {
     anzeige.textContent = zahlA += operator;
-        console.log("."+zahlA+". " + " --"+operator+"-- " + "Es ist ein Ops!")
-    anzeige.textContent = zahlA += zahlB;
-
-        console.log(aufgabe + zahlA + zahlB);
+    aufgabeA = anzeige.textContent;
+    console.log("Inhalt von aufgabeA ist: " + aufgabeA);
 }
 
+function calculateResult() {
+    aufgabe = document.getElementById('ergebnisBereich');
+    let aufgabeSplit = '';
+    aufgabeSplit = aufgabe.split(' ');
+    zahlA = aufgabeSplit[0];
+    operator = aufgabeSplit[1];
+    zahlB = aufgabeSplit[2];
+    
+    if (operator = ' +' ) {
+        addition(' + ');
+    } else if (operator = ' - ') {
+        subtrahieren(' - ');
+    } else if (operator = ' * ') {
+        multiplizieren(' * ');
+    } else if (operator = ' / ') {
+        dividieren(' / ');
+    }
 
-
-
-function calculateResult () {
-    let aufgabe = document.getElementById('ergebnisBereich');
-    let resultat = eval(aufgabe.innerHTML);
-    aufgabe.innerHTML = resultat;
 }
 
-function deleteLast () {
+function addition() {
+    console.log("Add " + aufgabeSplit);
+    Nummer1 = parseFloat(zahlA);
+    Nummer2 = parseFloat(zahlB);
+    console.log("Teil1: " + aufgabeSplit[0] + ", Teil2: " + aufgabeSplit[1] + " und Teil3: " + aufgabeSplit[2]);
+    resultat = Nummer1 + Nummer2;
+    console.log("Das ist das Ergebnis: " + resultat);
+}
+
+function subtrahieren() {
+    console.log("Sub " + aufgabeSplit);
+    Nummer1 = parseFloat(zahlA);
+    Nummer2 = parseFloat(zahlB);
+    console.log("Teil1: " + aufgabeSplit[0] + ", Teil2: " + aufgabeSplit[1] + " und Teil3: " + aufgabeSplit[2]);
+    resultat = Nummer1 - Nummer2;
+    console.log("Das ist das Ergebnis: " + resultat);
+}
+
+function multiplizieren() {
+    console.log("Mul " + aufgabeSplit);
+    Nummer1 = parseFloat(zahlA);
+    Nummer2 = parseFloat(zahlB);
+    console.log("Teil1: " + aufgabeSplit[0] + ", Teil2: " + aufgabeSplit[1] + " und Teil3: " + aufgabeSplit[2]);
+    resultat = Nummer1 * Nummer2;
+    console.log("Das ist das Ergebnis: " + resultat);
+}
+
+function dividieren() {
+    console.log("Div " + aufgabeSplit);
+    Nummer1 = parseFloat(zahlA);
+    Nummer2 = parseFloat(zahlB);
+    console.log("Teil1: " + aufgabeSplit[0] + ", Teil2: " + aufgabeSplit[1] + " und Teil3: " + aufgabeSplit[2]);
+    resultat = Nummer1 / Nummer2;
+    console.log("Das ist das Ergebnis: " + resultat);
+}
+
+function deleteLast() {
     let aufgabe = document.getElementById('ergebnisBereich');
-    if(aufgabe.innerHTML.endsWith(' ')) {
-        aufgabe.innerHTML = aufgabe.innerHTML.slice(0,-3);
-    }   else {
-        aufgabe.innerHTML = aufgabe.innerHTML.slice(0,-1);
+    if (aufgabe.innerHTML.endsWith(' ')) {
+        aufgabe.innerHTML = aufgabe.innerHTML.slice(0, -3);
+    } else {
+        aufgabe.innerHTML = aufgabe.innerHTML.slice(0, -1);
     }
 }
 
-function deleteAll () {
+function deleteAll() {
     let loeschen = document.querySelector('#ergebnisBereich');
-    let geloescht = delete(loeschen.innerHTML);
+    let geloescht = delete (loeschen.innerHTML);
     loeschen.innerHTML = geloescht;
     if (geloescht >= true) {
         document.querySelector('#ergebnisBereich').innerHTML = '',
-        zahlA = '', zahlB = '';
-    } 
+            zahlA = '', zahlB = ''; resultat = ''; aufgabeA = '';
+    }
 }
