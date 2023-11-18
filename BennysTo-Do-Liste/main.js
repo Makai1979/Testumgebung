3f8666a539116aa9a8e16ba7fc9452496d11efdd
Makai1979 <144675972+Makai1979@users.noreply.github.com>
2023-11-18 22:45:03 +0100

    Update 18.11.2023 - 22:44 Uhr

--- BennysTo-Do-Liste/main.js
+++ BennysTo-Do-Liste/main.js
@@ -3,60 +3,99 @@
 var tasks = [];
 
 $(document).ready(() => {
-    if(localStorage.getItem('tasks') != null)  // != -> Ungleich
+    if (localStorage.getItem('tasks') != null)  // != -> Ungleich
         tasks = JSON.parse(localStorage.getItem('tasks'));
 
     renderTasksTable();
+    pruefeDatum();
 
 })
 
-function createTask(){
+function createTask() {
     console.log("Create Task");
 
     let task = $('#coreInputTask').val();
     let begin = $('#coreInputBegin').val();
     let end = $('#coreInputEnd').val();
-    let OK = $('#coreInputDelete').val();
+    let del = $('#coreInputDelete').val();
+    let edit = $('#coreInputEdit').val();
 
-    let newTask = new Task (task, begin, end, OK);
+    let newTask = new Task(task, begin, end, del, edit);
     tasks.push(newTask);
 
-    localStorage.setItem('tasks',JSON.stringify(tasks));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
     renderTasksTable();
-
+    pruefeDatum();
 }
 
-function renderTasksTable(){
+function renderTasksTable() {
     // delete all rows that contain tasks
     $("table tr:gt(0)").remove();
 
     // render/create rows for all tasks
-    for(let i =0; i < tasks.length; i++){
+    for (let i = 0; i < tasks.length; i++) {
 
         $("tbody").append("<tr></tr>");
         let row = $("tr:last");
         $(row).append("<td id='thAufgabe'>" + tasks[i].task + "</td>");
-        $(row).append("<td id='thDate'>" + tasks[i].begin + "</td>");
-        $(row).append("<td id='thDate'>" + tasks[i].end + "</td>");
+        $(row).append("<td data-datum='tasks[i].begin.value' id='thDate'>" + tasks[i].begin + "</td>");
+        $(row).append("<td data-datum='tasks[i].end.value' id='thDate'>" + tasks[i].end + "</td>");
         $(row).append('<td id="thEnd"><button onclick="deleteTask(' + i + ')">Löschen</button></td>');
-
+        $(row).append('<td id="thEnd"><button onclick="editTask(' + i + ')">Edit</button></td>');
     }
 }
+
 
 class Task {
 
-    constructor (task, begin, end, OK) {
+    constructor(task, begin, end, del, edit) {
         this.task = task;
         this.begin = begin;
         this.end = end;
-        this.OK = OK;
+        this.del = del;
+        this.edit = edit;
     }
 
 }
 
-function deleteTask(index){  // index ist das i von dem erzeugten Task
-    tasks.splice(index, 1); 
+// Definiere die Funktion pruefeDatum
+function pruefeDatum() {
+    // Suche alle td-Elemente im DOM
+    const eintraege = document.querySelectorAll('td');
+
+    // Iteriere durch jedes td-Element
+    eintraege.forEach(eintrag => {
+        // Hole den Textinhalt des aktuellen td-Elements
+        const text = eintrag.textContent;
+
+        // Definiere ein Muster, um nach dem Datumsformat YYYY-MM-DD zu suchen
+        const datumPattern = /(\d{4})-(\d{2})-(\d{2})/; // YYYY-MM-DD pattern
+
+        // Suche nach dem Datumsmuster im Text des td-Elements
+        const match = text.match(datumPattern);
+
+        // Wenn ein Datumsmatch gefunden wurde
+        if (match) {
+            // Extrahiere das gefundene Datum im String-Format (z. B. "YYYY-MM-DD")
+            const datumString = match[0];
+
+            // Konvertiere das gefundene Datum in ein Date-Objekt
+            const zukuenftigesDatum = new Date(datumString);
+            const aktuellesDatum = new Date(); // Aktuelles Datum erhalten
+
+            // Vergleiche das gefundene Datum mit dem aktuellen Datum
+            if (zukuenftigesDatum < aktuellesDatum) {
+                // Wenn das gefundene Datum in der Vergangenheit liegt, ändere die Hintergrundfarbe des td-Elements in rot
+                eintrag.style.backgroundColor = 'red';
+            }
+        }
+    });
+}
+
+
+function deleteTask(index) {  // index ist das i von dem erzeugten Task
+    tasks.splice(index, 1);
     localStorage.setItem('tasks', JSON.stringify(tasks));
     renderTasksTable();
 }
