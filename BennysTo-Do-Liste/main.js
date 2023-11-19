48be9ee557aaaee038a9d9f325df52ba92da3fee
Makai1979 <144675972+Makai1979@users.noreply.github.com>
2023-11-19 02:22:41 +0100

    Update 19.11.2023 - 02:22 Uhr

--- BennysTo-Do-Liste/main.js
+++ BennysTo-Do-Liste/main.js
@@ -1,26 +1,19 @@
-
-
 var tasks = [];
 
 $(document).ready(() => {
-    if (localStorage.getItem('tasks') != null)  // != -> Ungleich
+    if (localStorage.getItem('tasks') != null)
         tasks = JSON.parse(localStorage.getItem('tasks'));
 
     renderTasksTable();
     pruefeDatum();
-
-})
+});
 
 function createTask() {
-    console.log("Create Task");
-
     let task = $('#coreInputTask').val();
     let begin = $('#coreInputBegin').val();
     let end = $('#coreInputEnd').val();
-    let del = $('#coreInputDelete').val();
-    let edit = $('#coreInputEdit').val();
 
-    let newTask = new Task(task, begin, end, del, edit);
+    let newTask = new Task(task, begin, end);
     tasks.push(newTask);
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -29,73 +22,82 @@ function createTask() {
     pruefeDatum();
 }
 
+function updateTask(index) {
+    const task = tasks[index];
+
+    task.task = $('#coreInputTask').val();
+    task.begin = $('#coreInputBegin').val();
+    task.end = $('#coreInputEnd').val();
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    renderTasksTable();
+    pruefeDatum();
+}
+
+function editTask(index) {
+    const task = tasks[index];
+
+    $('#coreInputTask').val(task.task);
+    $('#coreInputBegin').val(task.begin);
+    $('#coreInputEnd').val(task.end);
+
+    $('#coreInputMask').off('submit').on('submit', function(event) {
+        event.preventDefault();
+        updateTask(index);
+    });
+
+    $('#coreInputMask button[type="submit"]').text('Ändern');
+
+    pruefeDatum();
+}
+
 function renderTasksTable() {
-    // delete all rows that contain tasks
     $("table tr:gt(0)").remove();
 
-    // render/create rows for all tasks
     for (let i = 0; i < tasks.length; i++) {
-
-        $("tbody").append("<tr></tr>");
-        let row = $("tr:last");
-        $(row).append("<td id='thAufgabe'>" + tasks[i].task + "</td>");
-        $(row).append("<td data-datum='tasks[i].begin.value' id='thDate'>" + tasks[i].begin + "</td>");
-        $(row).append("<td data-datum='tasks[i].end.value' id='thDate'>" + tasks[i].end + "</td>");
-        $(row).append('<td id="thEnd"><button onclick="deleteTask(' + i + ')">Löschen</button></td>');
-        $(row).append('<td id="thEnd"><button onclick="editTask(' + i + ')">Edit</button></td>');
+        $("tbody").append(`<tr id="row${i}"></tr>`);
+        let row = $(`#row${i}`);
+        $(row).append(`<td>${tasks[i].task}</td>`);
+        $(row).append(`<td>${tasks[i].begin}</td>`);
+        $(row).append(`<td>${tasks[i].end}</td>`);
+        $(row).append(`<td><button onclick="deleteTask(${i})">Löschen</button></td>`);
+        $(row).append(`<td><button onclick="editTask(${i})">Bearbeiten</button></td>`);
     }
 }
 
-
 class Task {
-
-    constructor(task, begin, end, del, edit) {
+    constructor(task, begin, end) {
         this.task = task;
         this.begin = begin;
         this.end = end;
-        this.del = del;
-        this.edit = edit;
     }
-
 }
 
-// Definiere die Funktion pruefeDatum
-function pruefeDatum() {
-    // Suche alle td-Elemente im DOM
-    const eintraege = document.querySelectorAll('td');
-
-    // Iteriere durch jedes td-Element
-    eintraege.forEach(eintrag => {
-        // Hole den Textinhalt des aktuellen td-Elements
-        const text = eintrag.textContent;
-
-        // Definiere ein Muster, um nach dem Datumsformat YYYY-MM-DD zu suchen
-        const datumPattern = /(\d{4})-(\d{2})-(\d{2})/; // YYYY-MM-DD pattern
-
-        // Suche nach dem Datumsmuster im Text des td-Elements
-        const match = text.match(datumPattern);
-
-        // Wenn ein Datumsmatch gefunden wurde
-        if (match) {
-            // Extrahiere das gefundene Datum im String-Format (z. B. "YYYY-MM-DD")
-            const datumString = match[0];
-
-            // Konvertiere das gefundene Datum in ein Date-Objekt
-            const zukuenftigesDatum = new Date(datumString);
-            const aktuellesDatum = new Date(); // Aktuelles Datum erhalten
-
-            // Vergleiche das gefundene Datum mit dem aktuellen Datum
-            if (zukuenftigesDatum < aktuellesDatum) {
-                // Wenn das gefundene Datum in der Vergangenheit liegt, ändere die Hintergrundfarbe des td-Elements in rot
-                eintrag.style.backgroundColor = 'red';
-            }
-        }
-    });
-}
-
-
-function deleteTask(index) {  // index ist das i von dem erzeugten Task
+function deleteTask(index) {
     tasks.splice(index, 1);
     localStorage.setItem('tasks', JSON.stringify(tasks));
     renderTasksTable();
+    pruefeDatum();
+}
+
+function pruefeDatum() {
+    const eintraege = $('td');
+
+    eintraege.each(function() {
+        const text = $(this).text();
+        const datumPattern = /(\d{4})-(\d{2})-(\d{2})/;
+
+        const match = text.match(datumPattern);
+
+        if (match) {
+            const datumString = match[0];
+            const zukuenftigesDatum = new Date(datumString);
+            const aktuellesDatum = new Date();
+
+            if (zukuenftigesDatum < aktuellesDatum) {
+                $(this).css('backgroundColor', 'red');
+            }
+        }
+    });
 }
